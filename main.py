5cd44c31a40d0631133113c7b2b5fc93b2ffd653
majojos <33401919+majojos@users.noreply.github.com>
2018-07-05 21:24:54 +0200

    print added

--- main.py
+++ main.py
@@ -7,13 +7,17 @@ import module3
 
 def main():
     # Initialize Loggers in module
-    ##
+    #
+    #
+    #
     loggers = Logger('file.log')
 
     # Application code here
     module1.run()
     module2.run()
     module3.run()
+
+    print("test")
 
     # Logging summary
     print("Logging summary:")
